f645870d519e7b5e3dca9208884f18ab7c924828
Korben Lightfoot <korbykobs@gmail.com>
2024-09-01 14:33:17 +0100

    Everything

--- HBMF/AudioImporter.cs
+++ HBMF/AudioImporter.cs
@@ -1,49 +1,58 @@
-﻿using FMOD;
-using FMODUnity;
+﻿using HBMF.AudioImporter.Internal;
+using Il2CppFMOD;
+using Il2CppFMODUnity;
+using Il2CppInterop.Runtime.Attributes;
+using Il2CppInterop.Runtime.Injection;
+using MelonLoader;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 using UnityEngine;
 
-namespace AudioImporter
+namespace HBMF.AudioImporter
 {
-    public class AudioAPI
+    public class Audio
     {
-        public static AudioClip Import(Assembly assembly, string location)
+        public static AudioClip Import(byte[] bytes)
         {
-            return new AudioClip()
-            {
-                sound = ImportBytes(EmbeddedAssetBundle.LoadFromAssembly(assembly, location))
-            }; 
-        }
-        public static AudioClip Import(string fileLocation)
-        {
-            return new AudioClip()
-            {
-                sound = ImportBytes(File.ReadAllBytes(fileLocation))
-            };
+            AudioClip clip = new();
+            MelonCoroutines.Start(WaitAndImport(bytes, clip));
+            return clip;
         }
 
-        private static Sound ImportBytes(byte[] bytes)
+        public static AudioClip Import(string file)
         {
-            CREATESOUNDEXINFO exInfo = new CREATESOUNDEXINFO()
-            {
-                cbsize = System.Runtime.InteropServices.Marshal.SizeOf(typeof(CREATESOUNDEXINFO)),
-                length = (uint)bytes.Length
-            };
-            RuntimeManager.CoreSystem.createSound(bytes, MODE.LOOP_NORMAL | MODE.OPENMEMORY | MODE._3D, ref exInfo, out Sound sound);
-            return sound;
+            AudioClip clip = new();
+            MelonCoroutines.Start(WaitAndImport(file, clip));
+            return clip;
+        }
+
+        private static IEnumerator WaitAndImport(byte[] bytes, AudioClip clip)
+        {
+            yield return new WaitForCoreSystem();
+            string file = Path.GetTempFileName();
+            File.WriteAllBytes(file, bytes);
+            RuntimeManager.CoreSystem.createSound(file, MODE.LOOP_NORMAL | MODE._3D, out Sound sound);
+            clip.sound = sound;
+        }
+
+        private static IEnumerator WaitAndImport(string file, AudioClip clip)
+        {
+            yield return new WaitForCoreSystem();
+            RuntimeManager.CoreSystem.createSound(file, MODE.LOOP_NORMAL | MODE._3D, out Sound sound);
+            clip.sound = sound;
         }
     }
 
     public class AudioClip
     {
-        public Sound sound;
+        internal Sound sound;
     }
 
     public class AudioInstance
     {
-        public Channel channel;
+        internal Channel channel;
         public bool Use2D
         {
             get
@@ -53,14 +62,7 @@ namespace AudioImporter
             }
             set
             {
-                if (value == true)
-                {
-                    channel.setMode(MODE.LOOP_NORMAL | MODE.OPENMEMORY | MODE._2D);
-                }
-                else
-                {
-                    channel.setMode(MODE.LOOP_NORMAL | MODE.OPENMEMORY | MODE._3D);
-                }
+                channel.setMode(MODE.LOOP_NORMAL | MODE.OPENMEMORY | (value ? MODE._2D : MODE._3D));
             }
         }
         public bool Looping
@@ -72,14 +74,7 @@ namespace AudioImporter
             }
             set
             {
-                if (value == true)
-                {
-                    channel.setLoopCount(-1);
-                }
-                else
-                {
-                    channel.setLoopCount(0);
-                }
+                channel.setLoopCount(value ? -1 : 0);
             }
         }
         public float Volume
@@ -128,37 +123,39 @@ namespace AudioImporter
         }
     }
 
+    [RegisterTypeInIl2Cpp]
     public class AudioSource : MonoBehaviour
     {
         public AudioClip clip;
-        private VECTOR pos = new VECTOR();
-        private VECTOR vel = new VECTOR();
-        private VECTOR lastPos = new VECTOR();
-        private readonly List<AudioInstance> instances = new List<AudioInstance>();
+        private VECTOR pos = new();
+        private VECTOR vel = new();
+        private VECTOR lastPos = new();
+        private readonly List<AudioInstance> instances = new();
 
-        private void Update()
+        public AudioSource(IntPtr ptr) : base(ptr) { }
+
+        internal void Update()
         {
-            pos = new VECTOR
+            pos = new()
             {
                 x = transform.position.x,
                 y = transform.position.y,
                 z = transform.position.z
             };
-            vel = new VECTOR
+            vel = new()
             {
                 x = pos.x - lastPos.x,
                 y = pos.y - lastPos.y,
                 z = pos.z - lastPos.z
             };
             lastPos = pos;
-            List<AudioInstance> remove = new List<AudioInstance>();
-            foreach (AudioInstance instance in instances)
+            foreach (AudioInstance instance in instances.ToArray())
             {
-                if (instance.Attached == true)
+                if (instance.Attached)
                 {
                     instance.channel.set3DAttributes(ref pos, ref vel);
                 }
-                if (instance.UseSlowMotion == true)
+                if (instance.UseSlowMotion)
                 {
                     instance.channel.setPitch(instance.Pitch * Time.timeScale);
                 }
@@ -170,23 +167,17 @@ namespace AudioImporter
                 sound.getLength(out uint length, TIMEUNIT.MS);
                 if (length == 0)
                 {
-                    remove.Add(instance);
+                    instances.Remove(instance);
                 }
-            }
-            foreach (AudioInstance instance in remove)
-            {
-                instances.Remove(instance);
             }
         }
 
+        [HideFromIl2Cpp]
         public AudioInstance Play(PlaySettings playSettings = null)
         {
-            if (playSettings == null)
-            {
-                playSettings = new PlaySettings();
-            }
-            RuntimeManager.CoreSystem.playSound(clip.sound, new ChannelGroup(), true, out Channel newChannel);
-            AudioInstance audioInstance = new AudioInstance()
+            playSettings ??= new();
+            RuntimeManager.CoreSystem.playSound(clip.sound, new(), true, out Channel newChannel);
+            AudioInstance audioInstance = new()
             {
                 channel = newChannel,
                 Use2D = playSettings.Use2D,
@@ -201,6 +192,7 @@ namespace AudioImporter
             instances.Add(audioInstance);
             return audioInstance;
         }
+
         public void StopAll()
         {
             foreach (AudioInstance instance in instances)
@@ -208,6 +200,7 @@ namespace AudioImporter
                 instance.Stop();
             }
         }
+
         public void SetUse2DAll(bool use2D)
         {
             foreach (AudioInstance instance in instances)
@@ -215,6 +208,7 @@ namespace AudioImporter
                 instance.Use2D = use2D;
             }
         }
+
         public void SetLoopingAll(bool looping)
         {
             foreach (AudioInstance instance in instances)
@@ -222,6 +216,7 @@ namespace AudioImporter
                 instance.Looping = looping;
             }
         }
+
         public void SetVolumeAll(float volume)
         {
             foreach (AudioInstance instance in instances)
@@ -229,6 +224,7 @@ namespace AudioImporter
                 instance.Volume = volume;
             }
         }
+
         public void SetPitchAll(float pitch)
         {
             foreach (AudioInstance instance in instances)
@@ -236,6 +232,7 @@ namespace AudioImporter
                 instance.Pitch = pitch;
             }
         }
+
         public void SetUseSlowMotionAll(bool useSlowMotion)
         {
             foreach (AudioInstance instance in instances)
@@ -243,6 +240,7 @@ namespace AudioImporter
                 instance.UseSlowMotion = useSlowMotion;
             }
         }
+
         public void SetAttachedAll(bool attached)
         {
             foreach (AudioInstance instance in instances)
@@ -250,6 +248,7 @@ namespace AudioImporter
                 instance.Attached = attached;
             }
         }
+
         public void SetTimeAll(uint time)
         {
             foreach (AudioInstance instance in instances)
@@ -257,6 +256,7 @@ namespace AudioImporter
                 instance.Time = time;
             }
         }
+
         public void SetPauseAll(bool paused)
         {
             foreach (AudioInstance instance in instances)
@@ -265,7 +265,7 @@ namespace AudioImporter
             }
         }
 
-        private void OnDestroy()
+        internal void OnDestroy()
         {
             StopAll();
         }
@@ -280,5 +280,27 @@ namespace AudioImporter
         public bool UseSlowMotion = true;
         public bool Attached = true;
         public uint Time = 0;
+    }
+}
+
+namespace HBMF.AudioImporter.Internal
+{
+    [RegisterTypeInIl2Cpp]
+    public class WaitForCoreSystem : CustomYieldInstruction
+    {
+        public override bool keepWaiting
+        {
+            get
+            {
+                return !RuntimeManager.IsInitialized;
+            }
+        }
+
+        public WaitForCoreSystem(IntPtr ptr) : base(ptr) { }
+
+        public WaitForCoreSystem() : base(ClassInjector.DerivedConstructorPointer<WaitForCoreSystem>())
+        {
+            ClassInjector.DerivedConstructorBody(this);
+        }
     }
 }
